99a817752bbf80c0a7446ca89f3b46cec7455f4d
Panodil-abuser <57358213+mads546n@users.noreply.github.com>
2021-04-16 11:26:57 +0200

    Implementering af spiller-bevægelse

--- js/canvas.js
+++ js/canvas.js
@@ -2,6 +2,12 @@
 var canvas;
 var context; 
 
+//Input-variabler til bevægelse af spilleren
+var upKey;
+var rightKey;
+var downKey;
+var leftKey; 
+
 //Opstilling af variabler til spillet.
 var gameLoop;
 var player;
@@ -12,14 +18,57 @@ window.onload = function() {
     canvas = document.getElementById("game-canvas"); 
     context = canvas.getContext("2d"); 
 
-    //Variablen bruges til at tegne en hvid firkant på canvas.
-    context.fillStyle = "white"; 
-    context.fillRect(0, 0, 600, 600); 
+    //Opstilling af kontrol-taster.
+    setupInputs(); 
 
     //Skab spilleren.
-    player = new Player(50, 100); 
+    player = new Player(100, 400); 
 
-    //Start game loop
-    
+    //Start game loop. Kalder "skridt-funktionen" 30 gange i sekundet. Skaber illusion af bevægelse.
+    gameLoop = setInterval(skridt, 1000/30); 
+}
+
+function skridt() {
+    //Spillerens skridt
+    player.skridt();
+
+    //Draw'er alting. Kalder function "draw", da vi canvas skal tegnes, hver gang vi bevæger os.
+    draw();  
+}
+
+function draw() {
+    //Clear vores canvas, så spillerens, fjendernes osv. - placering konstant opdateres.
+    //Variablen bruges til at tegne en hvid firkant på canvas.
+    context.fillStyle = "white"; 
+    context.fillRect(0, 0, 600, 600);  
+
+    //Draw spilleren
+    player.draw();
+}
+//Opstiller kontrollerne, som styrer spillerens bevægelse. Hvis en af de følgende knapper nedtrykkes, ændres den tilhørende boolean-variables værdi. 
+function setupInputs() {
+    document.addEventListener("keydown", function(event) {
+        if (event.key === "w" || event.key === "ArrowUp") {
+            upKey = true; 
+        } else if (event.key === "a" || event.key === "ArrowLeft") {
+            leftKey = true; 
+        } else if (event.key === "s" || event.key === "ArrowDown") {
+            DownKey = true; 
+        } else if (event.key === "d" || event.key === "ArrowRight") {
+            rightKey = true; 
+        }
+    });
+
+    document.addEventListener("keyup", function(event) {
+        if (event.key === "w" || event.key === "ArrowUp") {
+            upKey = false; 
+        } else if (event.key === "a" || event.key === "ArrowLeft") {
+            leftKey = false; 
+        } else if (event.key === "s" || event.key === "ArrowDown") {
+            DownKey = false; 
+        } else if (event.key === "d" || event.key === "ArrowRight") {
+            rightKey = false; 
+        }
+    });
 }
 
--- js/player.js
+++ js/player.js
@@ -1,15 +1,52 @@
 function Player(x, y) {
-    this.x = x
-    this.y = y
-    this.height = 50
-    this.width = 100;
+    //Spiller-variabler
+    this.x = x; 
+    this.y = y;
+    //Horisontal velocitet. 
+    this.xspeed = 0; 
+    //Vertikal velocitet.
+    this.yspeed = 0; 
+    //Bestemmer hvor lang tid, der skal gå fra at knappen ikke længere er nedtrykket til, at spillerens bevægelse stoppes.
+    this.friction = 0.6;
+    //Maksimum fart spilleren kan opnå.
+    this.maxSpeed = 10;
+    this.height = 100; 
+    this.width = 50;
+    //
+    this.aktiv = true; 
     
-    this.skridt = function() {
+    this.skridt = function() {  
+        if(this.aktiv) {
+            //Horisontal bevægelse. Hvis spilleren ikke bevæger sig til enten en af siderne eller begge sider på samme tid, så vil spilleren ikke få en højere fart.
+            if (!leftKey && !rightKey || leftKey && rightKey) {
+                //Fart nedsættes
+                this.xspeed *=this.friction; 
+            } else if (rightKey) {
+                //Bevæg til højre
+                this.xspeed ++;
+            } else if (leftKey) {
+                //Bevæg til højre
+                this.xspeed --;
+            }
 
+            //Vertikal bevægelse
+
+            //Angivelse af fart
+            if (this.xspeed > this.MaxSpeed) {
+                this.xspeed = this.maxSpeed; 
+            } else if (this.xspeed < -this.maxSpeed) {
+                this.xspeed = -this.maxSpeed;
+            }
+
+            this.x += this.xspeed;
+            this.y += this.yspeed;
+        }
     }
 
     this.draw = function() {
-
+        //Angiver farven på spilleren
+        context.fillStyle = "red"; 
+        context.fillRect(this.x, this.y, this.width, this.height); 
     }
     
 }
