66e6f641eee797d073fc3f992b446197203a45a0
Panodil-abuser <57358213+mads546n@users.noreply.github.com>
2021-04-16 09:51:30 +0200

    Filupload

---
+++ player.js
@@ -0,0 +1,15 @@
+function Player(x, y) {
+    this.x = x
+    this.y = y
+    this.height = 50
+    this.width = 100;
+    
+    this.skridt = function() {
+
+    }
+
+    this.draw = function() {
+
+    }
+    
+}
